d7f24bff44da1a7f2f2eef765e2593d76bde964a
ori sharim <ori.sharim@gmail.com>
2023-11-04 02:45:43 +0200

    added cost func

--- Neural Network/src/NeuralNetwork/NeuralNetwork.java
+++ Neural Network/src/NeuralNetwork/NeuralNetwork.java
@@ -6,7 +6,7 @@ public class NeuralNetwork {
     
     private ArrayList<Layer> layers;
     private int amountOfInputs;
-    
+
     public NeuralNetwork(int amountOfInputs){
         this.amountOfInputs = amountOfInputs;
         layers = new ArrayList<Layer>();
@@ -28,5 +28,21 @@ public class NeuralNetwork {
         return inputs;
     }
 
+    public double getCost(double[] inputs, double[] expectedOutputs){
+        //make sure the amount of expectedOutputs entered is correct
+        if(inputs.length != expectedOutputs.length)
+            throw new RuntimeException("the given amount of expected outputs is wrong");
+        
+        double[] outputs = getOutputs(inputs);
+        double cost = 0;
+        for(int i = 0; i < outputs.length; i++){
+            //calc output error
+            double error = outputs[i] - expectedOutputs[i];
+            cost += error * error;
+        }
+
+        //return the avg cost
+        return cost / outputs.length;
+    }
 
 }
